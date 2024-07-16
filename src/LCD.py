06549b56553b596101bbaa9cf44c66b725eb44a2
Theo <theotasker@gmail.com>
2024-07-16 12:40:04 -0400

    mother of god

--- src/LCD.py
+++ src/LCD.py
@@ -67,7 +67,6 @@ if __name__ == '__main__':
         LCD_PINS_DATA = [26, 19, 13, 6]
 
         LCD = LCD(LCD_PIN_RS, LCD_PIN_RW, LCD_PIN_E, LCD_PINS_DATA, GPIO.BCM)
-        LCD.lcd.init()
         LCD.lcd.add_custom_char(0, [0x04])
 
         print('Testing LCD complete')
