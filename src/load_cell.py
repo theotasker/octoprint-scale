34a23d5ebc0971c3ee67c6e4680024ce687ce70c
Theo <theotasker@gmail.com>
2024-07-16 15:49:02 -0400

    dwdw

--- src/load_cell.py
+++ src/load_cell.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 
         print('Reading load cell values...')
         while True:
-            print(LoadCell.load_cell.get_raw_data())
+            print(LoadCell.load_cell.get_raw_data(times=4))
             sleep(0.5)
 
     except Exception as e:
