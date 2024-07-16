32a7630cd28865d0727757e67200e075ed1955a9
Theo <theotasker@gmail.com>
2024-07-16 11:56:25 -0400

    cycle time testing

--- src/raspberry_scale.py
+++ src/raspberry_scale.py
@@ -59,41 +59,46 @@ except Exception as e:
 ##############################################################################
 
 def main():
-    while True:
-        if Rotary.switch:
-            print('switch')
-            Rotary.switch = False
-            Rotary.change = 0
-            if LCD.current_option == 2:
-                if LCD.editing:
-                    LCD.editing = False
-                else:   
-                    LCD.editing = True
-                
-            elif LCD.current_option == 0:
-                LoadCell.zero()
-
-            elif LCD.current_option == 1:
-                LoadCell.set_add_mass += LCD.display_add_int
-
-        elif Rotary.change != 0:
-            print('change')
-            Rotary.change = 0
+    start_time = time.time()
+    if Rotary.switch:
+        print('switch')
+        Rotary.switch = False
+        Rotary.change = 0
+        if LCD.current_option == 2:
             if LCD.editing:
-                LCD.display_add_int += (Rotary.change * 10)
+                LCD.editing = False
+            else:   
+                LCD.editing = True
+            
+        elif LCD.current_option == 0:
+            LoadCell.zero()
 
-            else:
-                LCD.current_option += Rotary.change
-                if LCD.current_option > 2:
-                    LCD.current_option = 0
-                elif LCD.current_option < 0:
-                    LCD.current_option = 2
+        elif LCD.current_option == 1:
+            LoadCell.set_add_mass += LCD.display_add_int
 
-        current_weight = LoadCell.get_adjusted_weight()
-        LCD.update(current_weight)
+    elif Rotary.change != 0:
+        print('change')
+        Rotary.change = 0
+        if LCD.editing:
+            LCD.display_add_int += (Rotary.change * 10)
 
-        time.sleep(0.1)
-        print('cycle')
+        else:
+            LCD.current_option += Rotary.change
+            if LCD.current_option > 2:
+                LCD.current_option = 0
+            elif LCD.current_option < 0:
+                LCD.current_option = 2
+
+    current_weight = LoadCell.get_adjusted_weight()
+    LCD.update(current_weight)
+
+    cycle_time = time.time() - start_time
+    if cycle_time < 0.5:
+        sleep_time = 0.5 - cycle_time
+        print(f'Sleeping for {sleep_time} seconds')
+        time.sleep(sleep_time)
+    else:
+        print(f'Cycle time too long: {cycle_time}')
 
 
 ##############################################################################
@@ -102,7 +107,8 @@ def main():
 
 if __name__ == '__main__':
     try:
-        main()
+        while True:
+            main()
     except Exception as e:
         Rotary.stop()
         raise e
