30d933c6bd357dd635dc83c55aae6d5e608e42fa
Theo <theotasker@gmail.com>
2024-08-07 18:32:40 -0400

    trying manual pin selection

--- src/hsensor.py
+++ src/hsensor.py
@@ -1,12 +1,9 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import adafruit_dht
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D18)
+dhtDevice = adafruit_dht.DHT11(18)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
