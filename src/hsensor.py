f90ca46989851b6a19c7e07c3b7679c4d0af3ace
Theo <theotasker@gmail.com>
2024-08-07 18:33:36 -0400

    have to use "board" library apparently

--- src/hsensor.py
+++ src/hsensor.py
@@ -2,9 +2,6 @@ import time
 import board
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(18)
-
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
